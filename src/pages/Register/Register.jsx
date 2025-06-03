33b1023bd6c119ec2fbc9ec59d27385b85594a05
abdulkader33447 <abdulkader33447@gmail.com>
2025-06-04 01:04:04 +0600

    added firebase init,register form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Register = () => {
   return (
-    <div>
-      <h1>register</h1>
+    <div className="">
+      <div className="hero bg-base-200 min-h-[calc(100vh-284px)]">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+              <fieldset className="fieldset">
+                <h1 className="text-5xl font-bold">Register now!</h1>
+
+                <label className="label">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  className="input"
+                  placeholder="Name"
+                />
+
+                <label className="label">photoURL </label>
+                <input
+                  type="url"
+                  name="photoURL "
+                  className="input"
+                  placeholder="photoURL "
+                />
+
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="input"
+                  placeholder="Email"
+                />
+
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                {/* <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div> */}
+                <button className="btn btn-neutral mt-4">Login</button>
+                <p>Already have an account? please <Link to="/login" className="text-blue-600 underline">Login</Link></p>
+              </fieldset>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
+import AddArtifact from "../pages/AddArtifact/AddArtifact";
+import AllArtifacts from "../pages/AllArtifacts/AllArtifacts";
+import ArtifactDetails from "../pages/ArtifactDetails/ArtifactDetails";
+import MyArtifacts from "../pages/MyArtifacts/MyArtifacts";
+import UpdateArtifact from "../pages/UpdateArtifact/UpdateArtifact";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +17,34 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/addArtifacts",
+        element: <AddArtifact />,
+      },
+      {
+        path: "/allArtifacts",
+        element: <AllArtifacts />,
+      },
+      {
+        path: "/artifactDetails",
+        element: <ArtifactDetails />,
+      },
+      {
+        path: "/myArtifacts",
+        element: <MyArtifacts />,
+      },
+      {
+        path: "/updateArtifact",
+        element: <UpdateArtifact />,
       },
     ],
   },
